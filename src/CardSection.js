b116eef5f3124962df6cde661b9d7d114863d373
Antony Budianto <antonybudianto@gmail.com>
2019-10-04 22:04:03 +0700

    new card layout style

--- src/CardSection.js
+++ src/CardSection.js
@@ -3,60 +3,6 @@ import React from 'react';
 import './CardSection.css';
 
 class CardSection extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      selectedIndex: 0,
-      cardsInARow: 0
-    };
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    if (this.props.selected !== prevProps.selected) {
-      this.setState({
-        selectedIndex: 0
-      });
-    }
-  }
-
-  componentDidMount() {
-    this.updateWindowDimensions();
-    window.addEventListener('resize', this.updateWindowDimensions);
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('resize', this.updateWindowDimensions);
-  }
-
-  updateWindowDimensions = () => {
-    const width = window.innerWidth;
-    let cardsInARow;
-    switch (true) {
-      case width > 992:
-        cardsInARow = 5;
-        break;
-      case width > 768 && width < 992:
-        cardsInARow = 4;
-        break;
-      case width > 468 && width < 768:
-        cardsInARow = 2;
-        break;
-      default:
-        cardsInARow = 1;
-    }
-    this.setState({ cardsInARow });
-  };
-
-  incrementSelectedIndex = () => {
-    this.setState(({ selectedIndex }) => ({
-      selectedIndex: selectedIndex + 1
-    }));
-  };
-  decrementSelectedIndex = () => {
-    this.setState(({ selectedIndex }) => ({
-      selectedIndex: selectedIndex - 1
-    }));
-  };
   render() {
     const {
       header,
@@ -64,42 +10,9 @@ class CardSection extends React.Component {
       items,
       full,
       showLocation = true,
-      onClick = () => {},
-      profilesCount
+      onClick = () => {}
     } = this.props;
 
-    const { selectedIndex, cardsInARow } = this.state;
-    const style =
-      selectedIndex >= 1
-        ? { transform: `translateX(-${selectedIndex * 100}%)` }
-        : {};
-
-    let slidesCount;
-    if (cardsInARow) {
-      slidesCount = Math.ceil(profilesCount / cardsInARow);
-    }
-    const renderNavigation =
-      slidesCount >= 1 ? (
-        <div className="Navigation-wrapper">
-          <button
-            className={`Navigation-icon ${
-              selectedIndex === 0 ? 'disabled' : ''
-            }`}
-            onClick={this.decrementSelectedIndex}
-            disabled={selectedIndex === 0 ? true : false}
-          >
-            <i className="fa fa-chevron-left" aria-hidden="true"></i>
-          </button>
-          <button
-            className={`Navigation-icon ${
-              selectedIndex === slidesCount - 1 ? 'disabled' : ''
-            }`}
-            onClick={this.incrementSelectedIndex}
-          >
-            <i className="fa fa-chevron-right" aria-hidden="true"></i>
-          </button>
-        </div>
-      ) : null;
     return (
       <section className={'Card-section ' + (full ? 'full' : '')}>
         <div className="Card-header">
@@ -107,10 +20,9 @@ class CardSection extends React.Component {
             <h3>{header}</h3>
             <div className="Card-subheader">{subheader}</div>
           </div>
-          {renderNavigation}
         </div>
         <div className="Card-carousel">
-          <div className="Card-container" style={style}>
+          <div className="Card-container">
             {items.map((u, i) => {
               return (
                 <div
